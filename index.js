b7156ccd66368d5b14e236afdc3c75b1dc30ee85
Lisa Vinson <lisa@whitewhale.net>
2023-05-08 19:50:31 -0400

    added mobile overlay, menu transition, open/close menu icon

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 function toggleMobileMenu () {
-    console.log(this);
-    this.dataset.status === "open" ?  this.setAttribute("data-status", "closed")  : this.setAttribute("data-status", "open")  
+    const icon = this.querySelector("img");
+    const body = document.querySelector("body");
+
+    if (body.dataset.status === "open") {
+        body.setAttribute("data-status", "closed");
+        icon.setAttribute("src", "./assets/images/icon-menu.svg" )
+
+    } else {
+        body.setAttribute("data-status", "open");
+        icon.setAttribute("src", "./assets/images/icon-menu-close.svg" )
+
+    }
+     
 }
 
 const hamburgerIcon = document.querySelector(".navigation__mobile-icon");
